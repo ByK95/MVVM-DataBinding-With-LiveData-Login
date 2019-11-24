a495e061c7a80bb1ed2ae0fc5992a1abe3077b67
ByK95 <ByK95@github.com>
2019-11-24 21:31:49 +0300

    Add firebase checking

--- app/src/main/java/com/example/umangburman/databindingwithlivedata/ViewModel/LoginViewModel.java
+++ app/src/main/java/com/example/umangburman/databindingwithlivedata/ViewModel/LoginViewModel.java
@@ -2,16 +2,27 @@ package com.example.umangburman.databindingwithlivedata.ViewModel;
 
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
+import android.support.annotation.NonNull;
 import android.view.View;
 
 import com.example.umangburman.databindingwithlivedata.Model.LoginUser;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginViewModel extends ViewModel {
+
+    private FirebaseAuth firebaseAuth;
 
     public MutableLiveData<String> EmailAddress = new MutableLiveData<>();
     public MutableLiveData<String> Password = new MutableLiveData<>();
 
     private MutableLiveData<LoginUser> userMutableLiveData;
+
+    public void initFirebase(){
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
 
     public MutableLiveData<LoginUser> getUser() {
 
@@ -23,11 +34,22 @@ public class LoginViewModel extends ViewModel {
     }
 
     public void onClick(View view) {
+        LoginUser loginuser = new LoginUser(EmailAddress.getValue(),Password.getValue());
+        userMutableLiveData.setValue(loginuser);
+        boolean is_valid = !loginuser.validate();
+        if (is_valid){
+            firebaseAuth.signInWithEmailAndPassword(loginuser.getStrEmailAddress(),loginuser.getStrPassword()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
 
-        LoginUser loginUser = new LoginUser(EmailAddress.getValue(), Password.getValue());
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
 
-        userMutableLiveData.setValue(loginUser);
-
+                }
+            });
+        }
     }
 
 }
