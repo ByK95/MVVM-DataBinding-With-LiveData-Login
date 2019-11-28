1f201eef31805736ed646828b2f279d0cf95fd66
PhataCute <36421843+PhataCute@users.noreply.github.com>
2019-11-28 19:26:52 +0300

    mission abort

--- app/src/main/java/com/example/umangburman/databindingwithlivedata/View/DetailActivity.java
+++ app/src/main/java/com/example/umangburman/databindingwithlivedata/View/DetailActivity.java
@@ -1,24 +1,15 @@
 package com.example.umangburman.databindingwithlivedata.View;
 
-import android.arch.lifecycle.ViewModelProviders;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.umangburman.databindingwithlivedata.R;
-import com.example.umangburman.databindingwithlivedata.ViewModel.DetailViewModel;
 
 public class DetailActivity extends AppCompatActivity {
-
-    private DetailViewModel detailViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-
-        Intent intent = getIntent();
-
-        detailViewModel = ViewModelProviders.of(this).get(DetailViewModel.class);
     }
 }
--- app/src/main/java/com/example/umangburman/databindingwithlivedata/ViewModel/DetailViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.umangburman.databindingwithlivedata.ViewModel;
-
-import android.arch.lifecycle.ViewModel;
-
-public class DetailViewModel extends ViewModel {
-
-}
